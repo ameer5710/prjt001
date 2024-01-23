9bf9ef5fa68bbadcc2e17c1433ad27b9694bcf6b
ameer5710 <ameerhamzasmro5710@gmail.com>
2024-01-23 13:58:29 +0500

    first commit

---
+++ musab/src/app/about-us/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-red-900 text-white text-center text-3xl'>We are Muslims from Pakistan</div>
+  )
+}
+
+export default page
